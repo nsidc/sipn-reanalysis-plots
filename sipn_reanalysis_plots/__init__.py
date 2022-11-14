1e1f0f29d81b998d4af76c142915e50fb4f22f21
Matt Fisher <matt.fisher@nsidc.org>
2022-11-14 21:40:51 +0000

    Document how to run profiler

--- sipn_reanalysis_plots/__init__.py
+++ sipn_reanalysis_plots/__init__.py
@@ -1,7 +1,11 @@
 # isort: skip_file
+import logging
+
 import os
 
 from flask import Flask
+
+logger = logging.getLogger(__name__)
 
 
 app = Flask(__name__)
@@ -11,3 +15,12 @@ app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY', 'youcanneverguess'
 # NOTE: This is a circular import, but it's specified by the Flask docs:
 #     https://flask.palletsprojects.com/en/3.1.x/patterns/packages/
 import sipn_reanalysis_plots.routes  # noqa: E402, F401
+
+if os.environ.get('ENABLE_PROFILER'):
+    logger.info(f'Running profiler: {app.config}')
+
+    from werkzeug.middleware.profiler import ProfilerMiddleware
+    app.wsgi_app = ProfilerMiddleware(
+        app.wsgi_app,
+        profile_dir='./.prof',
+    )
