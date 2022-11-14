a3a8b5fe4a2dc984eb8a0c7d95c6a8c4f5672486
Matt Fisher <matt.fisher@nsidc.org>
2022-11-14 22:55:12 +0000

    Enable date selection

---
+++ sipn_reanalysis_plots/constants/epoch.py
@@ -0,0 +1,6 @@
+import datetime as dt
+
+# Start of period for which CFSR data is available:
+# EPOCH_START = dt.date(1979, 1, 1)
+# Temp:
+EPOCH_START = dt.date(1979, 1, 2)
--- sipn_reanalysis_plots/routes/index.py
+++ sipn_reanalysis_plots/routes/index.py
@@ -2,19 +2,39 @@ import base64
 import datetime as dt
 from io import BytesIO
 
-from flask import render_template
+from flask import render_template, request
 
 from sipn_reanalysis_plots import app
+from sipn_reanalysis_plots.constants.epoch import EPOCH_START
 from sipn_reanalysis_plots.util.plot import plot_cfsr_daily
 
 
 @app.route('/')
 def index():
-    fig = plot_cfsr_daily(dt.date(1979, 2, 1))
+    # TODO: Use real yesterday value once we have current test data
+    # yesterday = dt.date.today() - dt.timedelta(days=1)
+    yesterday = dt.date(EPOCH_START.year, 12, 31)
+
+    if request.args == {}:
+        return render_template(
+            'plot.html.j2',
+            date=yesterday,
+            min=EPOCH_START,
+            max=yesterday,
+        )
+
+    date = dt.datetime.strptime(request.args['date'], '%Y-%m-%d').date()
+    fig = plot_cfsr_daily(date)
 
     # Convert figure to bytes for embedding
     buf = BytesIO()
     fig.savefig(buf, format='png')
     img_data = base64.b64encode(buf.getbuffer()).decode('ascii')
 
-    return render_template('hello_world.html.j2', img_data=img_data)
+    return render_template(
+        'plot.html.j2',
+        date=date,
+        min=EPOCH_START,
+        max=yesterday,
+        img_data=img_data,
+    )
